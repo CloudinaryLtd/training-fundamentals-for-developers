6127d8a960cb6e0fa192f018edb41eef15d8cd12
Tamas Piros <tpiros@tpiros-macbook.local>
2019-04-24 12:16:37 +0200

    updates

--- upload/lesson1/example2.js
+++ upload/lesson1/example2.js
@@ -1,4 +1,11 @@
+require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+});
 
 cloudinary.uploader.upload('../../assets/images/kitten.jpg', {
         public_id: 'kitten'
--- upload/lesson1/example7.js
+++ upload/lesson1/example7.js
@@ -1,4 +1,12 @@
+require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+});
+
 
 cloudinary.uploader.upload('../../assets/images/kitten.jpg', {
         public_id: 'cute_animals/cute_cats/kitten'
