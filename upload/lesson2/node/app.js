12de58ea317ebb1d4b14c0d6df457d7eca046078
rebeccapeltz <rebecca.peltz@cloudinary.com>
2019-11-13 08:07:44 -0800

    add output showing current config

--- upload/lesson2/node/app.js
+++ upload/lesson2/node/app.js
@@ -5,6 +5,7 @@ if (typeof (process.env.CLOUDINARY_URL) === 'undefined') {
   console.warn('export CLOUDINARY_URL or set dotenv file');
 } else {
   console.log('cloudinary config:');
+  console.log(cloudinary.config());
 }
 
 const express = require('express');
