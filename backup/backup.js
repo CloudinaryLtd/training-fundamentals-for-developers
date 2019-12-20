6dcf7fc281fb63289f4bc6af9bc4f9118631d2dd
sambrace <41020072+sambrace@users.noreply.github.com>
2019-12-20 12:22:20 -0700

    Update backup.js

--- backup/backup.js
+++ backup/backup.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 
-cloudinary.uploader.upload('kitten3.jpg', {
-  backup: true
-})
-.then(restoreResult => console.log(restoreResult))
-.catch(error => console.error(error));
+cloudinary.uploader.upload('assets/images/giraffe.jpg', {
+    backup: true
+  })
+  .then(restoreResult => console.log(restoreResult))
+  .catch(error => console.error(error));
