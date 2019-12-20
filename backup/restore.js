0a552b840fdade1b5f54964fa38b51cb42fda47e
sambrace <41020072+sambrace@users.noreply.github.com>
2019-12-20 12:24:38 -0700

    Update restore.js

--- backup/restore.js
+++ backup/restore.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 
-cloudinary.api.restore(['car', 'motorcycle'])
+cloudinary.api.restore(['kitten', 'giraffe'])
 .then(restoreResult => console.log(restoreResult))
 .catch(error => console.error(error));
