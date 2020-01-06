7e3619e10b983b710fc4091da6591d0d893a7b11
sambrace <41020072+sambrace@users.noreply.github.com>
2020-01-06 15:29:47 -0700

    Update replace-tags.js

--- manage/replace-tags.js
+++ manage/replace-tags.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 
-cloudinary.uploader.replace_tag('animal', ['kitten', 'giraffe'])
+cloudinary.uploader.replace_tag('wild', ['kitten', 'giraffe'])
   .then(uploadResult => console.log(uploadResult))
   .catch(error => console.error(error));
