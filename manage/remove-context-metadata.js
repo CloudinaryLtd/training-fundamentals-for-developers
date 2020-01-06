cf287b6e081d54ab175fe839c67fcb9159e2fe7b
sambrace <41020072+sambrace@users.noreply.github.com>
2020-01-06 15:04:51 -0700

    Deleted due to depreciation.

--- manage/remove-context-metadata.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const cloudinary = require('cloudinary').v2;
-
-cloudinary.uploader.remove_all_context(['kitten', 'giraffe'])
-  .then(uploadResult => console.log(uploadResult))
-  .catch(error => console.error(error));
