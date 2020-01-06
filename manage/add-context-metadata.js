d3b1a69bec29f2d2a03231f42d70a6b0925426f0
sambrace <41020072+sambrace@users.noreply.github.com>
2020-01-06 15:04:36 -0700

    Deleted due to depreciation.

--- manage/add-context-metadata.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const cloudinary = require('cloudinary').v2;
-
-cloudinary.uploader.add_context('alt=Animal|class=Mammalia', ['kitten', 'giraffe'])
-  .then(uploadResult => console.log(uploadResult))
-  .catch(error => console.error(error));
