8dc3a3ce10a361cb89ef83b67ac02c052d384831
sambrace <41020072+sambrace@users.noreply.github.com>
2020-01-09 10:35:34 -0700

    Updated promises.

--- manage/search-2.js
+++ manage/search-2.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.search
-  .expression('resource_type:image AND uploaded_at>1d')
+  .expression('resource_type:image AND uploaded_at>1w')
   .sort_by('public_id', 'desc')
   .max_results(30)
-  .execute().then(result => console.log(result));
+  .execute()
+  .then(result => console.log(result))
+  .catch(error => console.error(error));
