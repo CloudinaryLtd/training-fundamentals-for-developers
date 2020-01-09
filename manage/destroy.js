4882dacedade905a5cdb1ff437084d2704bbcef7
sambrace <41020072+sambrace@users.noreply.github.com>
2020-01-09 14:20:01 -0700

    Updated promises.

--- manage/destroy.js
+++ manage/destroy.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.uploader.destroy('kitten')
-  .then(uploadResult => console.log(uploadResult))
+  .then(result => console.log(result))
   .catch(error => console.error(error));
