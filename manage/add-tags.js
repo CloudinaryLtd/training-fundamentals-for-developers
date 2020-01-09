1d36a1bed7e5693c2f5fff33f4ae6a22c5066991
sambrace <41020072+sambrace@users.noreply.github.com>
2020-01-09 14:21:26 -0700

    Updated promises.

--- manage/add-tags.js
+++ manage/add-tags.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.uploader.add_tag('animal', ['kitten', 'giraffe'])
-  .then(uploadResult => console.log(uploadResult))
+  .then(result => console.log(result))
   .catch(error => console.error(error));
