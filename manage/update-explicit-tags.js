4f0d94122727e9d48fa2650307a627f00939cca2
sambrace <41020072+sambrace@users.noreply.github.com>
2020-01-10 13:16:05 -0700

    Create update-explicit-tags.js

---
+++ manage/update-explicit-tags.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.uploader.explicit("kitten", {
+    type: 'upload',
+    tags: "baby"
+  })
+  .then(uploadResult => console.log(uploadResult))
+  .catch(error => console.error(error));
